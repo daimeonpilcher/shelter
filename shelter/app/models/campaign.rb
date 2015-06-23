f3259cb3b628308d0b3b85a6043deca4d3c258a7
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-06-23 12:12:30 -0700

    completed seed data, fixed controllers for has_many_through

--- shelter/app/models/campaign.rb
+++ shelter/app/models/campaign.rb
@@ -1,4 +1,5 @@
 class Campaign < ActiveRecord::Base
 	has_many :donations
 	has_many :campaign_clients
+	has_many :clients, through: :campaign_clients
 end
--- shelter/db/migrate/20150623171742_change_date_format_in_campaigns.rb
+++ shelter/db/migrate/20150623171742_change_date_format_in_campaigns.rb
@@ -1,5 +1,5 @@
 class ChangeDateFormatInCampaigns < ActiveRecord::Migration
   def change
-  	change_column :campaigns, :night_date,  :date
+  	change_column :campaigns, :night_date,  'date USING CAST(night_date AS date)'
   end
 end
