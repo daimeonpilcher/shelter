cfb778f0dc352e98a69a7a806135af13b68936b4
“Stephen <stephen@stepheneyer.comssh-keygen -t rsa -C stephen@stepheneyer.com>
2015-06-23 14:33:14 -0700

    Added save donation details to db

--- shelter/app/controllers/donations_controller.rb
+++ shelter/app/controllers/donations_controller.rb
@@ -9,25 +9,43 @@ class DonationsController < ApplicationController
 
 	def create
 		  	# Amount in cents
-  	@amount = 500
+  		@amount = 500
 
-	  customer = Stripe::Customer.create(
-    	:email => 'example@stripe.com',
-    	:card  => params[:stripeToken]
-  	)
+	  	customer = Stripe::Customer.create(
+    		:email => 'example@stripe.com',
+    		:card  => params[:stripeToken]
+  		)
 
-  	charge = Stripe::Charge.create(
-    	:customer    => customer.id,
-    	:amount      => @amount,
-    	:description => 'Rails Stripe customer',
-    	:currency    => 'usd'
-  	)
+  		charge = Stripe::Charge.create(
+    		:customer    => customer.id,
+    		:amount      => @amount,
+    		:description => 'Rails Stripe customer',
+    		:currency    => 'usd'
+  		)
 
-	rescue Stripe::CardError => e
-  	flash[:error] = e.message
-  	redirect_to charges_path
+  		current_amount = charge.amount
+  		current_transaction = charge.id
+  		new_donation = Donation.create(
+  			:user_id => current_user.id,
+  			:amount => current_amount,
+  			:transaction_id => current_transaction
+  		)
+		puts "Printing donation amount"
+		puts new_donation.amount
+		puts new_donation.user_id
+
+
+		rescue Stripe::CardError => e
+  		flash[:error] = e.message
+  		redirect_to charges_path
+	
+	
+	#donation.transaction_id = charge.receipt_number
+	
+	
 	end
-	# 	donation = current_user.donations.new(donation_params)
+
+	# donation = current_user.donations.new(donation_params)
 	# 	if donation.save
 	# 		redirect_to user_path(current_user)
 	# 	else
