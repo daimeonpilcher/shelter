81b58171f0c3eb94a29c49883ca530df9abc720e
Vanessa Farraye <vanessafarraye@Vanessas-MacBook-Air-2.local>
2015-06-23 17:14:57 -0700

    show clients

--- shelter/app/controllers/campaigns_controller.rb
+++ shelter/app/controllers/campaigns_controller.rb
@@ -1,2 +1,5 @@
 class CampaignsController < ApplicationController
+	def show
+		@client = Client.find(params[:id])
+	end
 end
--- shelter/app/controllers/clients_controller.rb
+++ shelter/app/controllers/clients_controller.rb
@@ -2,4 +2,8 @@ class ClientsController < ApplicationController
 	def show
 		@client = Client.find(params[:id])
 	end
+
+	def index
+		@clients = Client.all
+	end
 end
