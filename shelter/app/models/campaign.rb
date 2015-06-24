130c9b7974395ed46598b6f3ec4da7ef51f0e079
Stephen Eyer <stephen@stepheneyer.com>
2015-06-24 16:10:22 -0700

    Added has_many through for users and campaigns

--- shelter/app/models/campaign.rb
+++ shelter/app/models/campaign.rb
@@ -2,4 +2,18 @@ class Campaign < ActiveRecord::Base
 	has_many :donations
 	has_many :campaign_clients
 	has_many :clients, through: :campaign_clients
+	has_many :users, through: :donations
+
+	def amount_needed
+		amount_goal
+	end
+
+	def amount_raised
+		Donation.count * 25 #set fixed amount of $25 per room for now
+	end
+
+	def percentage_raised
+		(amount_raised / amount_needed) * 100
+	end
+
 end
